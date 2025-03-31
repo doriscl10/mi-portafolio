f189478bc9a0af75ac1643a84e9fa64c22606557
doris <doris.condori@gmail.com>
2025-03-31 18:00:26 -0500

    feat: add link to project card

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 document.getElementById("miBtn").addEventListener("click", function () {
     window.open("mi-cv.pdf", "_blank");});
+document.getElementById("btnDemo").addEventListener("click", function() {
+    window.open("https://vanile.fixa.digital", "_blank");
+});
