ccca5f327ff5105ef28e60e7c97daec4667bf1e0
doris <doris.condori10@gmail.com>
2025-06-24 18:15:28 -0500

    feat: update vanile page link

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 document.getElementById("miBtnCv").addEventListener("click", function () {
-    window.open("curriculum.pdf", "_blank");});
+  window.open("curriculum.pdf", "_blank");
+});
 document.getElementById("miBtnLn").addEventListener("click", function () {
-    window.open("https://www.linkedin.com/in/doris-condori-limachi/", "_blank");});
-document.getElementById("btnDemo").addEventListener("click", function() {
-    window.open("https://vanile.fixa.digital", "_blank");
+  window.open("https://www.linkedin.com/in/doris-condori-limachi/", "_blank");
 });
-document.getElementById("btnDemo2").addEventListener("click", function() {
-    window.open("https://doris-product-list.netlify.app/", "_blank");
+document.getElementById("btnDemo").addEventListener("click", function () {
+  window.open("https://vanile.fixa.pe/", "_blank");
 });
-document.getElementById("btnDemo3").addEventListener("click", function() {
-    window.open("https://hemolab.pe/", "_blank");
+document.getElementById("btnDemo2").addEventListener("click", function () {
+  window.open("https://doris-product-list.netlify.app/", "_blank");
+});
+document.getElementById("btnDemo3").addEventListener("click", function () {
+  window.open("https://hemolab.pe/", "_blank");
 });
